947750800feee33c110b4429ae2ebafff79f5098
System Administrator <root@Raphaels-MacBook-Air.local>
2019-10-25 11:58:09 -0300

    First commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import Container from '../../Components/Container/index';
+import { Form, SubmitButton, List } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    // carregar os dados do LocaoStorage
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      // se forem encontrados repositories:
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    // salvar os dados do localStorage
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+    this.setState({
+      repositories: [...repositories, data],
+      /* toda vez que o user adicionar um
+      repositório, um novo vetor é criado
+      baseado no vetor que já existe de repositórios
+
+       */
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, repositories, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository ${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
+<Link to={`/repository/${encodeURIComponent(repository.name)}`}></Link>
